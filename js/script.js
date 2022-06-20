e79ac5a5682b23a105fe4853ad56d5ca1efccc20
sai-monSVK <91848785+sai-monSVK@users.noreply.github.com>
2022-06-20 16:32:22 +0200

    contact_page_preview

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
 
 setInterval(function() {
-    let a = 2000;
+    let a = 4;
     let hodnota = parseInt(document.getElementById("starsBorn").innerHTML);
         document.getElementById("starsBorn").innerHTML = hodnota + a;
 
-    let b = 150;
+    let b = 30;
     let hodnotaDva = parseInt(document.getElementById("starsDied").innerHTML);
         document.getElementById("starsDied").innerHTML = hodnotaDva + b;
  
     let hodnotaTri = parseInt(document.getElementById("earthTravel").innerHTML);
         document.getElementById("earthTravel").innerHTML = hodnotaTri + b;
 
-    let c = 74;
+    let c = 14.8;
     let hodnotaStyri = parseInt(document.getElementById("universe").innerHTML);
         document.getElementById("universe").innerHTML = hodnotaStyri + c;
         
-}, 5000);
+}, 1000);
 
